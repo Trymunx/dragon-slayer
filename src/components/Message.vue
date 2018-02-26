cdeadeaa98410fd83e5b6c19495b80d13b15560a
Trymunx <trymunx@gmail.com>
2018-02-26 23:16:36 +0000

    WIP Stop using table to display messages

---
+++ src/components/Message.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="messages">
+    <div class="entity">{{entity}}</div>
+    <div class="message">{{message}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: ["entity", "message"]
+};
+</script>
+
+<style>
+.messages {
+  display: grid;
+  grid-template-columns: 160px 1fr;
+  grid-template-areas: "entity message";
+  overflow-y: auto;
+  min-width: 500px;
+}
+
+.entity {
+  grid-area: entity;
+  width: 140px;
+  vertical-align: top;
+  padding: 3px 10px;
+  color: var(--text-blur);
+  background-color: var(--ui-dark);
+  text-transform: capitalize;
+}
+
+.message {
+  grid-area: message;
+  width: 1fr;
+  vertical-align: top;
+  padding: 3px 10px;
+  margin: none;
+  color: var(--text);
+  background-color: var(--ui);
+}
+</style>
--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,41 +1,39 @@
 <template>
   <div id="output">
-    <div id="messagesBg"></div>
-    <output-table/>
+    <message v-for="(msg, i) in messages" :entity="msg.entity" :message="msg.message" :key="i"/>
+    <!-- <div id="messagesBg"></div> -->
   </div>
 </template>
 
 <script>
-import OutputTable from "./OutputTable";
+import Message from "./Message";
 
 export default {
   data() {
-    return {};
+    return {
+      messages: [
+        { entity: "Alpvax", message: "I'm helping you develop your game." },
+        { entity: "p-Dandy", message: "I'm fixing your broken shit." },
+        {
+          entity:
+            "This is a really long name that should break over a few lines",
+          message:
+            "I'm trying to help fix your broken shit. I'm trying to help fix your broken shit. I'm trying to help fix your broken shit. I'm trying to help fix your broken shit. I'm trying to help fix your broken shit."
+        },
+        { entity: "Me", message: "Thanks guys." }
+      ]
+    };
   },
   components: {
-    OutputTable
+    Message
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-:root {
-  /* UI colours */
-  --ui: #333231;
-  --ui-darker: #262626;
-  --ui-border: #1e1e1e;
-  --ui-dark: #2b2b2b;
-  /* Text colours */
-  --text: #daddd8;
-  --text-blur: #bababa;
-}
-
 #output {
   grid-area: output;
-  display: grid;
-  grid-template-columns: 160px 1fr;
-  grid-template-areas: "entity messages";
   overflow-y: auto;
   min-width: 500px;
 }
