336218774a359e0e50d3e334ac7634a51df60912
Trymunx <trymunx@gmail.com>
2018-03-02 00:28:42 +0000

    More messing around with context menu

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="context-menu">
+    <h4>Context Menu</h4>
       <li>1st item</li>
       <li>longer item goes here</li>
       <li>3rd item</li>
@@ -26,5 +27,11 @@ export default {};
   z-index: 1;
   border-radius: 5px;
   box-shadow: 0px 2px 15px -1px black;
+}
+
+h4 {
+  padding: 2px 4px 6px;
+  margin: 0px 2px 8px;
+  border-bottom: 1px solid var(--ui);
 }
 </style>
