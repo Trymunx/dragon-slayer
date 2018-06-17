4b714fa482626558eb2abeff994ef17a2f017cbd
Trymunx <trymunx@gmail.com>
2018-06-17 19:34:54 +0100

    Some ideas about how to parse commands and use gamestates

--- src/game/Commands/CommandParser.js
+++ src/game/Commands/CommandParser.js
@@ -8,3 +8,41 @@ export default {
     }
   }
 }
+
+
+// Should import game states here?
+// import StartGame from "../GameStates/StartGame";
+
+class CommandParser {
+  constructor() {
+    this.state = StartGame;
+  }
+
+  parse(command) {
+    // Moojs parsing things
+    let parsed = ["setName"]
+    // This part is hard
+    let response = this.state.parsed[command](parsed.args); // Doesn't work as command will be a string not a function
+    let response = this.state.command(parsed); // Instead of importing functions will have to have a switch in the gamestates
+    return response;
+  }
+
+  set state(state) {
+    this.state = state;
+  }
+}
+
+// export default new CommandParser;
+
+
+
+/* IDEAS
+Command parser will parse the text to find commands and arguments.
+Each game state will have to have a list of valid commands otherwise should
+return "Can't [command] [argument] [state.reason]".
+E.g. command = "walk", argument = "south", state = fight, state.reason = "while fighting".
+    "Can't walk south while fighting."
+
+States will have all of the state commands in them,
+so when importing the states they will export the commands.
+*/
