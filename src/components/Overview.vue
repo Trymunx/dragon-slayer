195c3e036105705216ba662984555593a8e9609e
trymunx <trymunx@gmail.com>
2018-07-25 23:46:48 +0100

    Use rot.js for displaying overview

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,17 +1,45 @@
 <template>
-  <div id="overview">
-    <canvas width="400" height="400" id="overview-canvas">
+  <div id="overview" @click="resizeOverview">
+    <!-- <canvas width="400" height="400" id="overview-canvas">
       Canvas unsupported, use a newer browser.
-    </canvas>
+    </canvas> -->
   </div>
 </template>
 
 <script>
 import DisplayMap from "../game/utils/DisplayMap";
+import * as ROT from "rot-js";
+import _ from "lodash";
 
 export default {
+  data() {
+    return {
+      display: new ROT.Display({
+        fontSize: 10,
+        fg: "#daddd8",
+        bg: "#1e1e1e",
+        forceSquareRatio: true
+      })
+    }
+  },
   mounted() {
-    DisplayMap();
+    // DisplayMap();
+
+    document.querySelector("#overview").appendChild(this.display.getContainer());
+
+    this.resizeOverview();
+
+    window.addEventListener("resize", _.debounce(this.resizeOverview, 100));
+  },
+  methods: {
+    resizeOverview() {
+      let overviewDiv = document.querySelector("#overview");
+
+      this.display.setOptions({
+        width: ~~(overviewDiv.offsetWidth / 10),
+        height: ~~(overviewDiv.offsetHeight / 10)
+      });
+    }
   }
 };
 </script>
@@ -22,16 +50,18 @@ export default {
 }
 
 #overview {
-  padding: 5px;
-  margin-bottom: 2px;
   color: var(--text-blur);
-  background-color: var(--ui-darker);
+  background-color: #1e1e1e;
   border-color: var(--ui-border);
   font-family: "Ubuntu Mono", monospace;
   font-size: 0.9em;
   white-space: pre-wrap;
   text-align: center;
   overflow-y: auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 #overview::-webkit-scrollbar {
