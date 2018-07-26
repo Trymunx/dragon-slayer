bd184d529cafb04f88c94c55526c78017e09800a
trymunx <trymunx@gmail.com>
2018-07-26 01:20:08 +0100

    Add rotjs display for game

---
+++ src/game/overview/Display.js
@@ -0,0 +1,12 @@
+import ROT from "rot-js";
+
+var display = new ROT.Display({
+  fontSize: 18,
+  fg: "#daddd8",
+  // bg: "#47681D",
+  bg: "#1e1e1e",
+  // bg: "#9e9e9e", // For debugging
+  forceSquareRatio: true
+});
+
+export default display;
