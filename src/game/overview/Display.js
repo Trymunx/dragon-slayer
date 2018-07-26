18cd0987e3046a739d4ffdf42f8abdedd1fdfcf6
trymunx <trymunx@gmail.com>
2018-07-26 13:45:48 +0100

    Move font size into a config file

--- src/game/overview/Display.js
+++ src/game/overview/Display.js
@@ -1,7 +1,8 @@
 import ROT from "rot-js";
+import config from "../config/display";
 
 var display = new ROT.Display({
-  fontSize: 18,
+  fontSize: config.fontSize,
   fg: "#daddd8",
   // bg: "#47681D",
   bg: "#1e1e1e",
