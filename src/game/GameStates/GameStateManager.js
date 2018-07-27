6c2d0fa3fb06285559ea34cb435eb3f4869d2fb7
trymunx <trymunx@gmail.com>
2018-07-27 23:51:34 +0100

    Process start game state and move between states

--- src/game/GameStates/GameStateManager.js
+++ src/game/GameStates/GameStateManager.js
@@ -1,9 +1,49 @@
-// This will handle starting the game and changing between game states
-export default {
+import GS_Start from "./GS_Start";
+import GS_Main from "./GS_Main";
+
+class GameStateManager {
+  constructor() {
+    this.state = null;
+  }
+
+  get currentState() {
+    return this.state;
+  }
+
+  set currentState(state) {
+    this.state = state;
+  }
+
   StartGame() {
-    return {
-      entity: "Welcome",
-      message: "Greetings adventurer. Please enter your name to begin, or leave it blank to have a name generated for you."
+    this.currentState = GS_Start;
+    // GS_Start.init();
+    this.state.init();
+  }
+
+  receiveInput(input) {
+    if (this.state) {
+      this.state.receiveInput(input);
     }
   }
+
+  nextState(caller, player) {
+    switch (caller) {
+      case "start":
+        this.currentState = GS_Main;
+        break;
+      case "main":
+        break;
+      case "end":
+        this.currentState = GS_Start;
+        break;
+      default:
+        console.error(`${caller} is not a registered game state.`);
+    }
+    this.state.setPlayer(player);
+    this.state.init();
+  }
 }
+
+let GS_Manager = new GameStateManager();
+
+export default GS_Manager;
