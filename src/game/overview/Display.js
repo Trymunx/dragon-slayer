2181c020c4199895f1f05a2487300aa3c55d6bd0
trymunx <trymunx@gmail.com>
2018-07-27 23:54:09 +0100

    Remove defaults that are overwritten during game state init

--- src/game/overview/Display.js
+++ src/game/overview/Display.js
@@ -1,10 +1,7 @@
 import ROT from "rot-js";
-import config from "../config/display";
 
 var display = new ROT.Display({
-  fontSize: config.fontSize,
   fg: "#daddd8",
-  // bg: "#47681D",
   bg: "#1e1e1e",
   // bg: "#9e9e9e", // For debugging
   forceSquareRatio: true
