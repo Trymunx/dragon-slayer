92bdb8df85e960cbaeec95b67d565e38d5fe514f
Trymunx <trymunx@gmail.com>
2019-05-07 22:11:47 +0100

    Alphabetise store objects

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -4,31 +4,85 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    messages: [],
-    player: {},
-    inputText: "",
-    commandMode: "text",
-    splash: true,
-    world: null,
-    creatures: {},
-    displayOrigin: {},
-    highlit: [],
+  actions: {
+    addCreature({ commit }, creature) {
+      commit("ADD_CREATURE", creature);
+    },
+    addMessage({ commit }, data) {
+      commit("ADD_MESSAGE", data);
+    },
+    enterCommand({ commit, state }, text) {
+      commit("ADD_MESSAGE", {
+        entity: state.playerName,
+        message: text,
+      });
+      commit("SET_INPUT_TEXT", "");
+    },
+    highlight({ commit }, tiles) {
+      if (tiles) {
+        commit("HIGHLIGHT_TILES", tiles);
+      } else {
+        commit("CLEAR_HIGHLIGHTED");
+      }
+    },
+    moveCreature({ commit }, data) {
+      commit("MOVE_CREATURE", data);
+    },
+    movePlayer({ commit }, dir) {
+      switch (dir.toUpperCase()) {
+        case "UP":
+          commit("MOVE_PLAYER", [0, -1]);
+          break;
+        case "DOWN":
+          commit("MOVE_PLAYER", [0, 1]);
+          break;
+        case "LEFT":
+          commit("MOVE_PLAYER", [-1, 0]);
+          break;
+        case "RIGHT":
+          commit("MOVE_PLAYER", [1, 0]);
+          break;
+      }
+    },
+    parseCommand(_, command) {
+      console.log("Parsing", command);
+    },
+    // receiveInput({ commit }, input) {
+    // gsMan.receiveInput(input);
+    // },
+    sendMessageAtPosition({ commit, state }, data) {
+      // Only send messages when player is there
+      if (state.player.pos.x === data.position[0] && state.player.pos.y === data.position[1]) {
+        commit("ADD_MESSAGE", { entity: data.entity, message: data.message });
+      }
+    },
+    setCommandMode({ commit }, mode) {
+      commit("SET_COMMAND_MODE", mode);
+    },
+    setInputText({ commit }, text) {
+      commit("SET_INPUT_TEXT", text);
+    },
+    setPlayer({ commit }, player) {
+      commit("SET_PLAYER", player);
+    },
+    setPlayerName({ commit }, name) {
+      commit("SET_PLAYER_NAME", name);
+    },
+    setSplash({ commit }, val) {
+      commit("SET_SPLASH", val);
+    },
+    setWorld({ commit }, world) {
+      commit("SET_WORLD", world);
+    },
+    setDisplayOrigin({ commit }, pos) {
+      commit("SET_DISPLAY_ORIGIN", pos);
+    },
+    // startGame({ commit }) {
+    // commit("START_GAME");
+    // },
   },
 
   getters: {
-    playerName: state => state.player.name,
-    messages: state => state.messages,
-    instantMode: state => (state.commandMode === "instant" ? true : false),
-    inputText: state => state.inputText,
-    splash: state => state.splash,
-    player: state => state.player,
-    playerPos: state => state.player.pos,
-    playerLevel: state => state.player.level,
-    displayOrigin: state => state.displayOrigin,
-    world: state => state.world,
-    worldExists: state => state.world !== null,
-    highlit: state => state.highlit,
     creatures: state => state.creatures,
     creaturesWithinRadius: state => (pos, radius = 10) => {
       if (!pos) {
@@ -47,6 +101,16 @@ const store = new Vuex.Store({
     creaturesAt: state => (x, y) => {
       return state.creatures[[x, y]];
     },
+    displayOrigin: state => state.displayOrigin,
+    highlit: state => state.highlit,
+    instantMode: state => (state.commandMode === "instant" ? true : false),
+    inputText: state => state.inputText,
+    messages: state => state.messages,
+    player: state => state.player,
+    playerLevel: state => state.player.level,
+    playerName: state => state.player.name,
+    playerPos: state => state.player.pos,
+    splash: state => state.splash,
     surroundings: state => (radius = 2) => {
       let surr = {
         items: {},
@@ -130,124 +194,11 @@ const store = new Vuex.Store({
 
       return surr;
     },
-  },
-
-  actions: {
-    setPlayerName({ commit }, name) {
-      commit("SET_PLAYER_NAME", name);
-    },
-    addMessage({ commit }, data) {
-      commit("ADD_MESSAGE", data);
-    },
-    sendMessageAtPosition({ commit, state }, data) {
-      // Only send messages when player is there
-      if (state.player.pos.x === data.position[0] && state.player.pos.y === data.position[1]) {
-        commit("ADD_MESSAGE", { entity: data.entity, message: data.message });
-      }
-    },
-    enterCommand({ commit, state }, text) {
-      commit("ADD_MESSAGE", {
-        entity: state.playerName,
-        message: text,
-      });
-      commit("SET_INPUT_TEXT", "");
-    },
-    setCommandMode({ commit }, mode) {
-      commit("SET_COMMAND_MODE", mode);
-    },
-    setInputText({ commit }, text) {
-      commit("SET_INPUT_TEXT", text);
-    },
-    startGame({ commit }) {
-      // commit("START_GAME");
-    },
-    receiveInput({ commit }, input) {
-      // gsMan.receiveInput(input);
-    },
-    parseCommand({ commit }, command) {
-      console.log("Parsing", command);
-    },
-    setPlayer({ commit }, player) {
-      commit("SET_PLAYER", player);
-    },
-    setWorld({ commit }, world) {
-      commit("SET_WORLD", world);
-    },
-    setSplash({ commit }, val) {
-      commit("SET_SPLASH", val);
-    },
-    movePlayer({ commit, state }, dir) {
-      switch (dir.toUpperCase()) {
-        case "UP":
-          commit("MOVE_PLAYER", [0, -1]);
-          break;
-        case "DOWN":
-          commit("MOVE_PLAYER", [0, 1]);
-          break;
-        case "LEFT":
-          commit("MOVE_PLAYER", [-1, 0]);
-          break;
-        case "RIGHT":
-          commit("MOVE_PLAYER", [1, 0]);
-          break;
-      }
-    },
-    setDisplayOrigin({ commit }, pos) {
-      commit("SET_DISPLAY_ORIGIN", pos);
-    },
-    addCreature({ commit }, creature) {
-      commit("ADD_CREATURE", creature);
-    },
-    moveCreature({ commit }, data) {
-      commit("MOVE_CREATURE", data);
-    },
-    highlight({ commit }, tiles) {
-      if (tiles) {
-        commit("HIGHLIGHT_TILES", tiles);
-      } else {
-        commit("CLEAR_HIGHLIGHTED");
-      }
-    },
+    world: state => state.world,
+    worldExists: state => state.world !== null,
   },
 
   mutations: {
-    SET_PLAYER_NAME(state, name) {
-      state.player.name = name;
-    },
-    ADD_MESSAGE(state, data) {
-      state.messages.push({
-        entity: data.entity,
-        message: data.message,
-      });
-    },
-    SET_COMMAND_MODE(state, mode) {
-      state.commandMode = mode;
-    },
-    SET_INPUT_TEXT(state, text) {
-      state.inputText = text;
-    },
-    START_GAME(state) {
-      console.log("Probably not going to work here buddy");
-    },
-    SET_PLAYER(state, player) {
-      state.player = player;
-    },
-    SET_WORLD(state, world) {
-      state.world = world;
-    },
-    SET_SPLASH(state, val) {
-      state.splash = val;
-    },
-    SET_DISPLAY_ORIGIN(state, pos) {
-      state.displayOrigin = pos;
-    },
-    MOVE_PLAYER(state, delta) {
-      const pos = {
-        x: state.player.pos.x + delta[0],
-        y: state.player.pos.y + delta[1],
-      };
-      state.player = Object.assign(state.player, { pos });
-    },
     ADD_CREATURE(state, creature) {
       if (state.creatures[creature.pos]) {
         state.creatures[creature.pos].push(creature);
@@ -255,6 +206,18 @@ const store = new Vuex.Store({
       } else {
         Vue.set(state.creatures, creature.pos, [creature]);
       }
+    },
+    ADD_MESSAGE(state, data) {
+      state.messages.push({
+        entity: data.entity,
+        message: data.message,
+      });
+    },
+    CLEAR_HIGHLIGHTED(state) {
+      state.highlit = [];
+    },
+    HIGHLIGHT_TILES(state, tiles) {
+      state.highlit = tiles;
     },
     MOVE_CREATURE(state, data) {
       if (state.creatures[data.newPos]) {
@@ -276,12 +239,49 @@ const store = new Vuex.Store({
         );
       }
     },
-    HIGHLIGHT_TILES(state, tiles) {
-      state.highlit = tiles;
+    MOVE_PLAYER(state, delta) {
+      const pos = {
+        x: state.player.pos.x + delta[0],
+        y: state.player.pos.y + delta[1],
+      };
+      state.player = Object.assign(state.player, { pos });
     },
-    CLEAR_HIGHLIGHTED(state) {
-      state.highlit = [];
+    SET_COMMAND_MODE(state, mode) {
+      state.commandMode = mode;
     },
+    SET_INPUT_TEXT(state, text) {
+      state.inputText = text;
+    },
+    SET_PLAYER(state, player) {
+      state.player = player;
+    },
+    SET_PLAYER_NAME(state, name) {
+      state.player.name = name;
+    },
+    SET_WORLD(state, world) {
+      state.world = world;
+    },
+    SET_SPLASH(state, val) {
+      state.splash = val;
+    },
+    SET_DISPLAY_ORIGIN(state, pos) {
+      state.displayOrigin = pos;
+    },
+    START_GAME() {
+      console.log("Probably not going to work here buddy");
+    },
+  },
+
+  state: {
+    commandMode: "text",
+    creatures: {},
+    displayOrigin: {},
+    highlit: [],
+    inputText: "",
+    messages: [],
+    player: {},
+    splash: true,
+    world: null,
   },
 });
 
