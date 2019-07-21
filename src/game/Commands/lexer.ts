efed01680dbd7cd62beccb16746e827fbc99ad6b
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-07-21 09:40:57 +0100

    :truck: Move dictionary and its population to its own module.

--- src/game/Commands/lexer.ts
+++ src/game/Commands/lexer.ts
@@ -1,42 +1,8 @@
 import { Token, TokenStream, TokenType, isGameCommand, isPlayerCommand, isConjunction, isNumber} from './types/Token'
 import { Result, ok, err } from './types/Result'
+
+import { dictionary } from './dictionary'
 import { matchTest } from './templates'
-
-// Dictionary ------------------------------------------------------------------
-// Use these types / enums / things as a dictionary for all the game's terms.
-// Something like two consecutive tokens:
-//   [ { type: TokenType.Word, value: "leather" }
-//   , { type: TokenType.Word, value: "hide" }
-//   ]
-// can be converted into a single *item* token with the value "leather hide".
-import creatures from '../entities/CreaturesTemplates.json'
-import gameItems from '../items/gameItems'
-
-// Keen readers will notice that a Dictionary actually is the same type alias as
-// as a TokenStream. Their use is very different, however, and so the types
-// remain separate to make clear their intended use.
-type Dictionary =
-  Token[]
-
-let dictionary: Dictionary = [
-  { type: TokenType.Direction, value: "north" },
-  { type: TokenType.Direction, value: "south" },
-  { type: TokenType.Direction, value: "west" },
-  { type: TokenType.Direction, value: "east" }
-]
-
-// Populate the dictionary with all the create names in the game. We use
-// Object.values instead of a tradtional for..in to appease typescript...
-Object.values(creatures).forEach(({ name }) => {
-  dictionary.push({ type: TokenType.Creature, value: name, })
-})
-
-// We need both the singular and plural names for all the items in the game,
-// the player might want to drop two *swords* or just one *sword*.
-gameItems.forEach(({ name, plural }) => {
-  dictionary.push({ type: TokenType.Item, value: name })
-  dictionary.push({ type: TokenType.Item, value: plural })
-})
 
 // Tokenise --------------------------------------------------------------------
 const tokenise = (input: string[]): TokenStream => {
