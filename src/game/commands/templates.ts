511af8f25f02b5c6c281f8a6fd7150241fa8031f
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-07-22 22:10:11 +0100

    :sparkles: More descriptive errors for template matching.

--- src/game/commands/templates.ts
+++ src/game/commands/templates.ts
@@ -1,23 +1,48 @@
 import { any, hasType, isExact, isPositive, Template } from "./predicate";
 import { emptyToken, TokenStream, TokenType } from "./types/Token";
-import { Err, err, isErr, isOk, ok, Result } from "./types/Result";
+import { err, isErr, isOk, mapErr, ok, Result } from "./types/Result";
 
 // Types -----------------------------------------------------------------------
-export type MatchResult =
-  [TokenStream, Template]
+export type MatchSuccess = {
+  stream: TokenStream,
+  template: Template
+}
 
-export type MatchError =
-  string
+export type MatchFailure = {
+  message: string
+  type: MatchErrorType,
+}
 
-const NO_MATCHES = "I couldn't find any commands that matched this sequence";
-const UNRECGONISED_INTIAL_TOKEN = "The first token in this sequence is not a " +
-  "command I recognise.";
-const TOO_MANY_ARGUMENTS = (command: string, got: string) =>
-  "While parsing " + command + " I received an extra " + got + ".";
+export enum MatchErrorType {
+  NO_MATCHES,
+  UNRECGONISED_INTIAL_TOKEN,
+  TOO_MANY_ARGUMENTS,
+  TOKEN_TYPE_MISMATCH
+}
+
+const noMatchesError = () => ({
+  message: "I couldn't find any commands that matched this sequence.",
+  type: MatchErrorType.NO_MATCHES,
+});
+
+const unrecognisedInitialTokenError = () => ({
+  message: "The first token in this sequence is not a command I recognise.",
+  type: MatchErrorType.UNRECGONISED_INTIAL_TOKEN,
+});
+
+const tooManyArgumentsError = (command: string, got: string) => ({
+  message: `While parsing ${command} I received an extra ${got}.`,
+  type: MatchErrorType.TOO_MANY_ARGUMENTS,
+});
+
+const tokenTypeMismatchError = (message: string) => ({
+  message,
+  type: MatchErrorType.TOKEN_TYPE_MISMATCH,
+});
 
 // This type alias just helps to slim down some of the function signatures
 // below.
-type R = Result<MatchError, MatchResult>
+type R = Result<MatchFailure, MatchSuccess>
 
 // GameCommand templates -------------------------------------------------------
 const gameCommandTemplates: Template[] = [
@@ -33,6 +58,7 @@ const playerCommandTemplates: Template[] = [
   [isExact("drop"), isPositive, hasType(TokenType.Item)],
   [any([isExact("fight"), isExact("attack")]), hasType(TokenType.Creature)],
   [any([isExact("run"), isExact("flee")])],
+  [isExact("examine"), any([hasType(TokenType.Item), hasType(TokenType.Creature)])],
 ];
 
 // Matching templates ----------------------------------------------------------
@@ -41,7 +67,7 @@ const matchTemplate = (tokenStream: TokenStream, template: Template): R => {
   const [initialPredicate] = template;
 
   if (isErr(initialPredicate(initialToken))) {
-    return err(NO_MATCHES);
+    return err(noMatchesError());
   } else if (tokenStream.length === 1 && template.length === 1) {
     return ok([tokenStream, template]);
   } else {
@@ -56,22 +82,25 @@ const matchTemplate = (tokenStream: TokenStream, template: Template): R => {
       // This if statements handles the case where our command input is longer
       // than the template so there is no predicate to run.
       if (predicate === undefined) {
-        return err(TOO_MANY_ARGUMENTS(initialToken.value as string, token.type));
+        return err(tooManyArgumentsError(initialToken.value, token.type));
 
         // This else if handles the case where our command input was shorter than
         // the template. We can pass in a special empty token to the current
         // predicate and be confident it will Err out. This will give us a nice
         // error message telling us what token it actually expected!
       } else if (token === undefined) {
-        return predicate(emptyToken) as Err<string>;
+        return mapErr(predicate(emptyToken), tokenTypeMismatchError);
       }
 
       const result = predicate(token);
 
-      if (isErr(result)) return result;
+      if (isErr(result)) return mapErr(result, tokenTypeMismatchError);
     }
 
-    return ok([tokenStream, template]);
+    return ok({
+      stream: tokenStream,
+      template,
+    });
   }
 };
 
@@ -83,7 +112,7 @@ export const match = (tokenStream: TokenStream): R => {
   const [firstToken] = tokenStream;
 
   if (firstToken.type !== TokenType.GameCommand && firstToken.type !== TokenType.PlayerCommand) {
-    return err(UNRECGONISED_INTIAL_TOKEN);
+    return err(unrecognisedInitialTokenError());
   }
 
   const templates = firstToken.type === TokenType.GameCommand
@@ -93,7 +122,7 @@ export const match = (tokenStream: TokenStream): R => {
   const match = templates
     .map(template => matchTemplate(tokenStream, template))
     // Remove all entries where the command didn't even *begin* parsing.
-    .filter(a => !(isErr(a) && a.value === NO_MATCHES))
+    .filter(a => !(isErr(a) && a.value.type === MatchErrorType.NO_MATCHES))
     // Move all Ok results to the beginning of the array, so that when we shift
     // if there *was* an Ok result we're guaranteed to get it. We still want
     // to preserve the errors, however, which is why we don't simply filter the
@@ -101,5 +130,5 @@ export const match = (tokenStream: TokenStream): R => {
     .sort((a, b) => isOk(a) && isErr(b) ? -1 : isErr(a) && isOk(b) ? 1 : 0)
     .shift();
 
-  return match || err(NO_MATCHES);
+  return match || err(noMatchesError());
 };
